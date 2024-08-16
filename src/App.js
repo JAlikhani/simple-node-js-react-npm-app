0f23a0c9d06d787cd5be79c598d14903b615706a
https://github.com/JAlikhani/my-react-test <alikhani.j2al@gmail.com>
2024-08-16 17:38:09 +1000

    Make Mehrnoosh edit in App.js

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ class App extends Component {
           <h1 className="App-title">Welcome to React</h1>
         </header>
         <p className="App-intro">
-          Jalal Edition <code>src/App.js</code> and save to reload.
+          Mehrnoosh Edition <code>src/App.js</code> and save to reload.
         </p>
       </div>
     );
